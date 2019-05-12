4b64874b349d872c5a0f6855d63aa06c05fbd891
mwaiyusuf <mwaigalo5@gmail.com>
2019-05-12 11:37:01 +0300

    adding urls

--- images/views.py
+++ images/views.py
@@ -11,4 +11,17 @@ def home(request):
   location = Location.objects.all()
   category = Categories.objects.all()
 
+  if 'location'  in request.GET and request.GET['location']:
+    name = request.GET.get('location')
+    images = Image.view_location(name)
+
+  elif 'category' in request.GET and request.GET['category']:
+    name = request.GET.get('categories')
+    images = Image.view_category(name)
+    return render(request, 'all-images.html', {"name":name,"images":images,"name":name})
+
+  return render(request,"all-images.html",{"images":images,"category":category,"location":location})
+
+    
+
 
