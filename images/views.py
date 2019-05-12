1900bb7eb7daa3a46b78a614c0da3aad8048a5b7
mwaiyusuf <mwaigalo5@gmail.com>
2019-05-12 11:10:14 +0300

    home request

--- images/views.py
+++ images/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-from django.http  import HttpResponse  #returning response to user
+from django.http  import HttpResponse,Http404,HttpResponseRedirect #returning response to user
+from .models import *
+
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the Moringa Tribune')
+
+def home(request):
+  images = image.objects.all()
+  location = Location.objects.all()
+  category = Categories.objects.all()
+
+
