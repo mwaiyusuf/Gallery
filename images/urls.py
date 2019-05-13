e25935483d0a7572fa6b8027631ff1fcb7a42651
mwaiyusuf <mwaigalo5@gmail.com>
2019-05-13 17:02:09 +0300

    url search

--- images/urls.py
+++ images/urls.py
@@ -5,8 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.home,name = 'home'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^image/(\d+)',views.get_image_by_id,name ='image')
 
 ]
-# if settings.DEBUG:
-#     # urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-#     urlpatterns+= static(settings.MEDIA_URL)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
