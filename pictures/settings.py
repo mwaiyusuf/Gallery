ba3ebb96f58e902e4729d6fdf1926f58d190d205
mwaiyusuf <mwaigalo5@gmail.com>
2019-05-14 12:57:50 +0300

    bootstrap 3

--- pictures/settings.py
+++ pictures/settings.py
@@ -59,8 +59,8 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # Application definition
 
 INSTALLED_APPS = [
-    'images',
     'bootstrap3',
+    'images',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
