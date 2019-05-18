51d6a0217ea1132ef54183a7181c14d22db3e458
mwaiyusuf <mwaigalo5@gmail.com>
2019-05-18 12:53:30 +0300

    increasing the images size

--- images/models.py
+++ images/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Categories(models.Model):
     name=models.CharField(max_length=30)
-#returning a unicode of anny object
+#returning a unicode of anny object      object 2 string
     def __str__(self):
         return self.name
 
